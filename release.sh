0f8000f364f6174875446680524c0c0ec0c1f944
Benno Straub <benno.straub@outlook.de>
2022-02-20 22:34:06 +0100

    Changed release process?

--- release.sh
+++ release.sh
@@ -1,23 +1,9 @@
-#!/bin/sh -e
-
-PORT="5000"
+#!/bin/bash
 
 rm -r dist .parcel-cache || true
 npx parcel build  src/index.html || true
 npx parcel build src/index.html
 
-npx serve -l "${PORT}" dist/ &
-echo hi
-
-sleep 0.3
-
-npx inliner "http://localhost:${PORT}" > index.html
-
-for x in $(jobs -p); do kill $x; done;
-
-wait
-clear
-
-git reset
-git add ./index.html
-git commit -m "Release"
+rm ./*.html ./*.css ./*.js
+cp dist/*.html dist/*.css dist/*.js .
+git add ./*.js ./*.html ./*.css
