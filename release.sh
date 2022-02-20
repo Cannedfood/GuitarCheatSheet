7ce2e7fc6080623b59b05f5f287be044265c33e0
Benno Straub <benno.straub@outlook.de>
2022-02-20 23:17:44 +0100

    Fixed release

--- release.sh
+++ release.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
+rm *.html *.css *.js || true
 rm -r dist .parcel-cache || true
-npx parcel build  src/index.html --public-url ./ || true
-npx parcel build src/index.html --public-url ./
 
-rm ./*.html ./*.css ./*.js
+npx parcel build src/index.html --public-url ./ --no-scope-hoist || true
+npx parcel build src/index.html --public-url ./ --no-scope-hoist
+
 cp dist/*.html dist/*.css dist/*.js .
-git add ./*.js ./*.html ./*.css
+git add *.js *.html *.css
