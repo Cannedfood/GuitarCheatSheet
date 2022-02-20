1fd0eeb0c60ac2ff40df0b6895965c65100af3bc
Benno Straub <benno.straub@outlook.de>
2022-02-20 22:52:56 +0100

    Updated public-url option

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -r dist .parcel-cache || true
-npx parcel build  src/index.html || true
-npx parcel build src/index.html
+npx parcel build  src/index.html --public-url ./ || true
+npx parcel build src/index.html --public-url ./
 
 rm ./*.html ./*.css ./*.js
 cp dist/*.html dist/*.css dist/*.js .
