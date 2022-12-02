2f026ed978a50f4cf41afbd0fe86c2c319238d5a
Benno Straub <benno.straub@outlook.de>
2022-12-02 21:35:58 +0100

    Don't generate dist/assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import path from 'path'
 
 export default defineConfig({
+    build: {
+        assetsDir: '',
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
